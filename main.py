ee879fb77934659db3227a754b04058c71e18a1a
Oskar Østby <oskar.oestby@gmail.com>
2018-06-18 15:25:08 +0200

    Fixes a few bugs and adds Norwegian names.

--- main.py
+++ main.py
@@ -4,31 +4,42 @@ import time as t
 from shutil import copyfile
 
 mem_stick_path = "F:/DCIM/"
-
 picture_path = "C:/Users/Gro/Pictures"
 
+def translateMonth(month_number):
+    # Translates month number to a Norwegian month name
+    months = ["Januar", "Februar", "Mars", "April", "Mai"
+              "Juni", "Juli", "August", "September", "Oktober",
+              "November", "Desember" ]
+    return months[month_number]
+ 
 def getFolderName():
+    # Creates the correct folder path
     year = t.strftime("%Y")
-    month = t.strftime("%B")
+    month = translateMonth(int(t.strftime("%m")))
     return "/" + year + "/" + month
 
 def createFolders(path_to_folder):
+    # Creates folder paths that are needed.
     if not os.path.exists(path_to_folder):
-        print("Creating new folder")
+        print("Lager nye mapper...")
         os.makedirs(path_to_folder)
 
 
-
 def main():
+    global mem_stick_path
+    global picture_path
+
     # First checks for the existence of the memory stick
     if not os.path.exists(mem_stick_path):
         print("Finner ikke minnepenn!")
+        print("Har du huska å koble den til?")
         exit()
     else:
         os.chdir(mem_stick_path)
     
     # Creates a new path for pictures and then creates the correct folders.
-    picture_path.append(getFolderName())
+    picture_path += getFolderName()
     createFolders(picture_path)
     
     # Retrieves a list of all folders in the memory stick.
@@ -36,5 +47,10 @@ def main():
     for d in mem_stick_folders:
         work_dir = mem_stick_path + "/" + d
         os.chdir(work_dir)
+        print("Jobber i mappe:", work_dir)
+
         for p in glob.glob("*.jpg"):
+            print("Kopierer bilde: ", p)
             copyfile(work_dir + "/" + p, picture_path + "/" + p)
+
+main()
