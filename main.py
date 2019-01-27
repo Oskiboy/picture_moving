f0a9a902ba7673430aa056677e8be30ce2079ede
Oskar Østby <31510521+Oskiboy@users.noreply.github.com>
2019-01-27 22:25:49 +0100

    Adds folder structure and new files for what is to be a way for the program to update itself

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import glob
 import time as t
 from shutil import copyfile
 
+VERSION = (1, 1)    # Version number (MAJOR, MINOR)
 mem_stick_path = "F:/DCIM/"
 picture_path = "C:/Users/Gro/Pictures"
 
 def translateMonth(month_number):
-    # Translates month number to a Norwegian month name
+    """
+     Translates month number to a Norwegian month name
+    """
     months = ["Januar", "Februar", "Mars", "April", "Mai",
               "Juni", "Juli", "August", "September", "Oktober",
               "November", "Desember" ]
     return months[month_number - 1]
  
 def getFolderName():
-    # Creates the correct folder path
+    """
+     Creates the correct folder path
+    """
     year = t.strftime("%Y")
     month = translateMonth(int(t.strftime("%m")))
     return "/" + year + "/" + month
 
 def createFolders(path_to_folder):
-    # Creates folder paths that are needed.
+    """
+     Creates folder paths that are needed.
+    """
     if not os.path.exists(path_to_folder):
         print("Lager nye mapper...")
         os.makedirs(path_to_folder)
@@ -30,15 +37,20 @@ def main():
     global mem_stick_path
     global picture_path
 
-    # First checks for the existence of the memory stick
+    # If the memory stick is not found the script cannot do anything.
     if not os.path.exists(mem_stick_path):
         print("Finner ikke minnepenn!")
         print("Har du huska å koble den til?")
+        # This is to make the error readable in a windows terminal.        
+        t.sleep(10)
         exit()
     else:
         os.chdir(mem_stick_path)
     
-    # Creates a new path for pictures and then creates the correct folders.
+    """
+    We want to store the pictures in a easy to understand folderstructure,
+    basically Pictures/year/month.
+    """
     picture_path += getFolderName()
     createFolders(picture_path)
     
