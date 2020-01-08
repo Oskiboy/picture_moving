3f19d4bfac58d73867b00d4b55c92e674c922643
oskaro <oskar.oestby@gmail.com>
2020-01-08 14:12:47 +0100

    Clean up
    Clean up some messy code.
    
    Make file globbing a bit better.

--- main.py
+++ main.py
@@ -5,76 +5,70 @@ import shutil
 import subprocess
 from shutil import copyfile
 
+MEM_STICK_PATH = "F:/DCIM/"
+PICTURE_PATH = "D:/Users/Gro/Pictures"
+CANNOT_FIND_CARD_ERROR = '#' * 40 +"""
 
-mem_stick_path = "F:/DCIM/"
-picture_path = "D:/Users/Gro/Pictures"
+Finner ikke minnekort!
+Har du huska å koble den til?
+Eventuelt prøv å bytte USB port.
+Trykk enter for å avslutte.
+
+""" + '#'*40
 
 def translateMonth(month_number):
-    # Translates month number to a Norwegian month name
+    """Translates month number to a Norwegian month name"""
     months = ["Januar", "Februar", "Mars", "April", "Mai",
               "Juni", "Juli", "August", "September", "Oktober",
               "November", "Desember" ]
     return months[month_number - 1]
  
 def getFolderName():
-    # Creates the correct folder path
+    """Creates the correct folder path"""
     year = t.strftime("%Y")
     month = translateMonth(int(t.strftime("%m")))
     return os.path.join(year, month)
 
 def createFolders(path_to_folder):
-    # Creates folder paths that are needed.
+    """ Creates folder paths that are needed."""
     if not os.path.exists(path_to_folder):
         print("Lager nye mapper...")
         os.makedirs(path_to_folder)
 
-def copyFiles(source, dest):
-    mem_stick_folders = glob.glob(
-        os.path.join(source, "*")
-    )
-    for d in mem_stick_folders:
-        work_dir = os.path.join(source, d)
-        print("Jobber i mappe:", work_dir)
-
-        print("\nKopierer bilder...")
-        for p in glob.glob(os.path.join(work_dir,"*.jpg")):
-            print("Kopierer bilde: ", p)
+def copyFiles(source, dest, formats=['jpg', 'JPG', 'AVI', 'avi']):
+    """Copy all files from the source to the destination."""
+    
+    for f in formats:
+        # Creates a GLOB of the source_path/**/*.${FORMAT}
+        files = mem_stick_folders = glob.glob(
+            os.path.join(source, "**", "*."+f)
+        )
+        for media_file in files:
+            print("Kopierer fil: ", media_file)
             copyfile(
-                p,
-                os.path.join(dest, os.path.basename(p))
+                media_file,
+                os.path.join(dest, os.path.basename(media_file))
             )
-
-        print("\nKopierer filmer...")
-        for m in glob.glob(os.path.join(work_dir, '*.avi')):
-            print("Kopierer film: ", m)
-            copyfile(
-                m,
-                os.path.join(dest, os.path.basename(m))
-            )
-
 
 def main():
-    global mem_stick_path
-    global picture_path
-
     # First checks for the existence of the memory stick
-    if not os.path.exists(mem_stick_path):
-        print("Finner ikke minnepenn!")
-        print("Har du huska å koble den til? Trykk enter for å avslutte.")
-        input()
+    if not os.path.exists(MEM_STICK_PATH):
+        print(CANNOT_FIND_CARD_ERROR)
         exit()
 
     # Creates a new path for pictures and then creates the correct folders.
-    picture_path = os.path.join(picture_path, getFolderName())
-    createFolders(picture_path)
+    output_path = os.path.join(PICTURE_PATH, getFolderName())
+    print(output_path)
+    createFolders(output_path)
     
     # Retrieves a list of all folders in the memory stick.
-    copyFiles(mem_stick_path, picture_path)
+    copyFiles(MEM_STICK_PATH, output_path)
 
     print("\nOverføring var vellyket!")
     
 def update():
     """A crude way to update the local git repo."""
+    currdir = os.getcwd()
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
@@ -82,13 +76,14 @@ def update():
     git = shutil.which('git')
     proc = subprocess.run([git, 'fetch'], stdout=subprocess.PIPE)
     proc = subprocess.run([git, 'pull'], stdout=subprocess.PIPE)
-    print("Update result:", proc.stdout)
+    print("Update result:", proc.stdout.decode('UTF-8'))
+    os.chdir(currdir)
 
 
 if __name__ == '__main__':
-    print("Oppdaterer:")
+    print("#"*40 + "\nOppdaterer:")
     update()
     print()
     
-    print("Kjører overføring:")
+    print("#"*40 + "\nKjører overføring:")
     main()
